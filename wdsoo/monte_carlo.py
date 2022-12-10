5ef6528921a31e80042079c78ddf5d556b470599
GalPerelman <peligur@gmail.com>
2022-12-10 17:48:08 +0200

    update monte carlo simulations and sampling

--- wdsoo/monte_carlo.py
+++ wdsoo/monte_carlo.py
@@ -1,36 +1,35 @@
+import os
 import numpy as np
 from numpy.random import default_rng
-import pandas as pd
 import matplotlib.pyplot as plt
 
-np.set_printoptions(linewidth=np.inf)
+# local imports
+from . import uncertainty_utils as uutils
+from .uncertainty_utils import UCategory, UElement
+
+np.set_printoptions(threshold=np.inf)
 
 
 class MC:
-    def __init__(self, sim, n_sim, gamma, delta, rho):
+    def __init__(self, sim, n_sim, gamma, rho):
         self.sim = sim
         self.n_sim = n_sim
         self.gamma = gamma
-        self.delta = delta
         self.rho = rho
 
+        self.t = self.sim.num_steps
         self.epsilon = 0.0001
+        self.udata = uutils.init_uncertainty(os.path.join(self.sim.data_folder, 'uncertainty', 'uncertainty.json'))
         self.num_tanks = len(self.sim.network.tanks)
 
-    def run(self, distribution, plot=False):
-        cases = {'multivariate': self.multivariate_sample(),
-                 'uniform': self.uniform_sample(),
-                 'normal': self.normal()}
-
-        sample, demands = cases[distribution]
-        # sample, demands = self.multivariate_sample()
-        # sample, demands = self.uniform_sample()
-        # sample, demands = self.normal()
+    def run(self, sample_function, plot=False):
+        sample, demands = sample_function(self)
 
         sim_violations = []
         for i, (tank_name, tank) in enumerate(self.sim.network.tanks.items()):
             tank_inflow = self.sim.network.tanks[tank_name].vars['inflow'].values.reshape(-1, 1)
-            res = (tank.initial_vol + (tank_inflow - sample[:, :, i].T).cumsum(axis=0))
+
+            res = (tank.initial_vol + (tank_inflow - sample[i * self.t: (i + 1) * self.t, :]).cumsum(axis=0))
             tank_violations = self.get_violations_for_tank(tank, res)
             sim_violations.append(tank_violations)
 
@@ -38,16 +37,16 @@ class MC:
         sim_violations = np.count_nonzero(sim_violations == 1, axis=0)  # counts number of violations in each sample
         sim_violations = np.array([np.ones(sim_violations.shape), sim_violations]).min(axis=0)
         if plot:
-            self.plot(sample)
+            self.plot(sample, sum(sim_violations)/self.n_sim)
         return sum(sim_violations)/self.n_sim
 
-    def plot(self, sample):
+    def plot(self, sample, vr):
         fig, axes = plt.subplots(nrows=2, ncols=int(len(self.sim.network.tanks) / 2 + 1), sharex=True, figsize=(12, 6))
         axes = axes.ravel()
 
         for i, (tank_name, tank) in enumerate(self.sim.network.tanks.items()):
             tank_inflow = self.sim.network.tanks[tank_name].vars['inflow'].values.reshape(-1, 1)
-            res = (tank.initial_vol + (tank_inflow - sample[:, :, i].T).cumsum(axis=0))
+            res = (tank.initial_vol + (tank_inflow - sample[i * self.t: (i + 1) * self.t, :]).cumsum(axis=0))
 
             #  Visualizing the sample
             res = np.vstack([np.ones((1, self.n_sim)) * tank.initial_vol, res])
@@ -58,8 +57,19 @@ class MC:
             axes[i].plot(range(len(tank.vars) + 1), [tank.initial_vol] + tank.vars['value'].to_list(), c='k')
             axes[i].grid()
 
-        # plt.suptitle(f'Monte Carlo simulation\nViolations rate: {(sum(sim_violations) / self.n_sim):.2f}')
+        plt.suptitle(f'Monte Carlo simulations - Violations rate: {vr*100:.0f}')
         plt.tight_layout()
+
+    def plot_sample(self, sample, demands):
+        fig, axes = plt.subplots(nrows=2, ncols=int(len(self.sim.network.tanks) / 2 + 1), sharex=True, figsize=(12, 6))
+        axes = axes.ravel()
+        t = demands.shape[1]
+
+        for i, (tank_name, tank) in enumerate(self.sim.network.tanks.items()):
+            axes[i].set_title(tank_name)
+            axes[i].plot(sample[i * t: (i + 1) * t, :], c='C0', alpha=0.4)
+            axes[i].plot(demands[i, :], c='k')
+            axes[i].grid()
 
     def get_violations_for_tank(self, tank, res):
         z_max_vol = np.where(res - tank.max_vol <= self.epsilon, 0, 1)  # 1 for constraint violation, 0 otherwise
@@ -87,15 +97,27 @@ class MC:
     def normal(self):
         demands = np.array([t.vars['demand'].values for t in self.sim.network.tanks.values()])
         demands = demands.T
-        zeros_idx = np.where(~demands.all(axis=0))
+        time_steps, n_tanks = demands.shape
+        non_zero_idx = np.argwhere(demands.any(axis=0)).flatten()
+        demands = demands[:, demands.all(axis=0)]
+        dem_tanks = demands.shape[1]
 
         zeta = np.random.normal(0, 1, size=(demands.shape[0], demands.shape[1], self.n_sim))
-        cov = get_cov_by_correlation(demands, self.rho, norm=False)
+        cov = uutils.observations_cov(demands, self.rho)
 
         d = np.linalg.cholesky(cov)
         sample = d @ zeta
         sample = sample.reshape(self.n_sim, demands.shape[0], demands.shape[1])
         sample = demands + sample
+
+        A = np.zeros(shape=(self.n_sim, time_steps, n_tanks))
+        B = np.zeros(shape=(time_steps, n_tanks))
+        for i, idx in enumerate(list(non_zero_idx)):
+            A[:, :, idx] = sample[:, :, i]
+            B[:, idx] = demands[:, i]
+
+        sample = A
+        demands = B
         return sample, demands.T
 
     def uniform_sample(self):
@@ -106,7 +128,7 @@ class MC:
         zeta = np.random.normal(0, 1, size=(demands.shape[0], demands.shape[1], self.n_sim))
         y = np.apply_along_axis(lambda x: x / np.sqrt(np.sum(x ** 2)), axis=0, arr=zeta) * self.gamma
 
-        cov = get_cov_by_correlation(demands, self.rho)
+        cov = uutils.observations_cov(demands, self.rho)
         d = np.linalg.cholesky(cov)
 
         sample = d @ y
@@ -116,110 +138,56 @@ class MC:
         return sample, demands.T
 
     def multivariate_sample(self):
+        rng = np.random.default_rng()
         demands = np.array([t.vars['demand'].values for t in self.sim.network.tanks.values()])
         demands = demands.T
-        zeros_idx = np.where(~demands.all(axis=0))
+        t, n_tanks = demands.shape
+        non_zero_idx = np.argwhere(demands.any(axis=0)).flatten()
 
-        cov = get_cov_by_correlation(demands, self.rho).T
-        rng = np.random.default_rng()
-        sample = rng.multivariate_normal(np.zeros(demands.shape[1]), cov, size=(demands.shape[0], self.n_sim),
-                                         method='cholesky')
+        sigma = self.udata['demand'].sigma
+        z = rng.multivariate_normal(np.zeros(sigma.shape[0]), sigma, size=self.n_sim, method='cholesky').T
 
-        sample = sample.reshape(self.n_sim, demands.shape[0], demands.shape[1])
-        sample = demands * (1 + sample)
-        sample[:, :, zeros_idx] = 0
+        z = np.random.normal(0, 1, size=((sigma.shape[0], self.n_sim)))
+        z = self.udata['demand'].delta @ z
+
+        nominal = demands.T.reshape(-1, 1)
+        sample = np.zeros((t * n_tanks, self.n_sim))
+
+        for i, idx in enumerate(list(non_zero_idx)):
+            sample[idx * t: (idx + 1) * t] = nominal[idx * t: (idx + 1) * t] + z[i * t: (i + 1) * t]
+
         return sample, demands.T
 
     def multivariate_in_ellipse(self):
+        rng = np.random.default_rng()
         demands = np.array([t.vars['demand'].values for t in self.sim.network.tanks.values()])
         demands = demands.T
-        zeros_idx = np.where(~demands.all(axis=0))
+        t, n_tanks = demands.shape
+        non_zero_idx = np.argwhere(demands.any(axis=0)).flatten()
 
-        Z = np.random.normal(0, 1, size=(self.n_sim, demands.shape[0], demands.shape[1]))
-        Z = np.apply_along_axis(lambda x: x / np.sqrt(np.sum(x ** 2)), axis=0, arr=Z)
-        z = (Z[:, :, 5].T)
-        D = np.linalg.cholesky(cov)
-        sample = Z @ D.T
+        z = np.random.normal(0, 1, size=((sigma.shape[0], self.n_sim)))
+        z = np.apply_along_axis(lambda x: x / np.sqrt(np.sum(x ** 2)), axis=0, arr=z)
+        z = self.udata['demand'].delta @ z
 
+        nominal = demands.T.reshape(-1, 1)
+        sample = np.zeros((t * n_tanks, self.n_sim))
 
-def get_poitive_defined_cov(demands):
-    """ observation (hours) in rows, features (demands zones) in columns - rowvar=False"""
-    cov = np.cov(demands, rowvar=False)
-    return cov
+        for i, idx in enumerate(list(non_zero_idx)):
+            sample[idx * t: (idx + 1) * t] = nominal[idx * t: (idx + 1) * t] + z[i * t: (i + 1) * t]
 
-
-
-def plot_multivariate_sample(demands, sample):
-    import math
-    fig, axes = plt.subplots(nrows=2, ncols=math.ceil(demands.shape[1] / 2), sharex=True, figsize=(12, 6), sharey=True)
-    axes = axes.ravel()
-
-    for i in range(demands.shape[1]):
-        axes[i].plot(sample[:, :, i].T, c='C0', alpha=0.3)
-        axes[i].plot(demands[:, i], c='k')
-        axes[i].grid()
-    plt.tight_layout()
+        return sample, demands.T
 
 
 if __name__ == "__main__":
-    res = pd.read_csv('Tank 7.csv', sep=',', header=None).values
-    max_vol, min_vol = 1179, 620.7  # tank 8
-    max_vol, min_vol = 500, 200  # tank 1
-
-    print(res)
-    z_max_vol = np.where(res - max_vol < 0, 0, 1)  # 1 for constraint violation, 0 otherwise
-    print(z_max_vol)
-    n_max_vol = np.count_nonzero(z_max_vol == 1, axis=0)  # counts number of violations in each sample
-    print(n_max_vol)
-    n_max_vol = np.array([np.ones(n_max_vol.shape), n_max_vol]).min(axis=0)  # 1 if any violation in the sample
-    print(n_max_vol)
-
-
-
-
-
-
-
-
-
-
-# #     demands = np.loadtxt('demands.csv', delimiter=',').T
-# #     cov = get_cov_by_correlation(demands, 0.8).T
-# #     N = 50
-# #
-# #     Z = np.random.normal(0, 1, size=(N, 24, 5))
-# #     # Z = np.apply_along_axis(lambda x: x / np.sqrt(np.sum(x ** 2)), axis=0, arr=Z)
-# #     print(np.mean(Z))
-# #     print(np.std(Z))
-# #     D =np.linalg.cholesky(cov)
-# #     A = Z@D.T
-# #     print(np.mean(A), np.std(A))
-# #     A = demands + A
-# #     A[A < 0] = 0
-# #     plot_multivariate_sample(demands, A)
-# #
-# #     rng = np.random.default_rng()
-# #     sample = rng.multivariate_normal(np.zeros(demands.shape[1]), cov, size=(demands.shape[0], N), method='cholesky')
-# #     sample = sample.reshape(N, demands.shape[0], demands.shape[1])
-# #     sample = demands + sample
-# #     sample[sample < 0] = 0
-# #     total_dem = (sample.sum(axis=1).sum(axis=1))
-# #     plot_multivariate_sample(demands, sample)
-# #
-# #     plt.show()
-
-
-###############################
-    # std = demands.std(axis=1)
-    # sigma = np.zeros((num_tanks, num_tanks))
-    # # np.fill_diagonal(sigma, np.full(shape=(num_tanks, 1), fill_value=self.delta))
-    # np.fill_diagonal(sigma, std)
-    # corr = self.constant_correlation_mat(num_tanks, 0.8)
-    # cov = sigma @ corr @ sigma
-
-    # zeta = np.random.normal(loc=0, scale=1, size=(self.sim.num_steps, self.n_sim, demands.shape[0]))
-    # y = (zeta / sum(np.sqrt(zeta ** 2))) * self.gamma
-    # D = np.linalg.cholesky(cov)
+    pass
+    # res = pd.read_csv('Tank 7.csv', sep=',', header=None).values
+    # max_vol, min_vol = 1179, 620.7  # tank 8
+    # max_vol, min_vol = 500, 200  # tank 1
     #
-    # sample = demands.mean(axis=1) + y @ D
-    # sample.reshape((self.n_sim, demands.shape[0], self.sim.num_steps))
+    # print(res)
+    # z_max_vol = np.where(res - max_vol < 0, 0, 1)  # 1 for constraint violation, 0 otherwise
+    # print(z_max_vol)
+    # n_max_vol = np.count_nonzero(z_max_vol == 1, axis=0)  # counts number of violations in each sample
+    # print(n_max_vol)
+    # n_max_vol = np.array([np.ones(n_max_vol.shape), n_max_vol]).min(axis=0)  # 1 if any violation in the sample
+    # print(n_max_vol)
