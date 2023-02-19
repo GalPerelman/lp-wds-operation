c8406afcffbbf59155f3ad1ec140fe35fb5cd30d
GalPerelman <peligur@gmail.com>
2023-02-19 18:51:00 +0200

    fix time steps aggregation feature

--- wdsoo/simulation.py
+++ wdsoo/simulation.py
@@ -5,16 +5,16 @@ import datetime
 import time
 
 # local imports
+from . import utils
 from . import demands
 from .sim_results import Results
-from . import graphs
-from . import utils
+from . import electricity
 from . import opt
 
 pd.set_option('display.max_columns', 999)
 pd.set_option('display.width', 1000)
 pd.set_option('display.float_format', lambda x: '%.3f' % x)
-
+pd.options.mode.chained_assignment = 'raise'
 
 class Simulation:
     def __init__(self, data_folder, network, start_time, end_time, hr_step_size=1, dynamic_min_vol=False):
@@ -27,14 +27,14 @@ class Simulation:
 
         self.duration = self.get_duration()
         self.time_range = self.get_time_range()
-        self.num_steps = self.get_num_steps()
         self.tariff_epsilon = 0.0002
         self.network = network
         self.vars = pd.DataFrame()
 
-        self.date_tariff = utils.vectorized_tariff(self.data_folder, self.time_range)
-        self.date_tariff = utils.split_range(self.date_tariff, self.hr_step_size)
+        self.date_tariff = electricity.vectorized_tariff(self.data_folder, self.time_range)
+        self.date_tariff = self.split_range(self.date_tariff, self.hr_step_size)
         self.time_range = self.date_tariff.index
+        self.num_steps = self.get_num_steps()
 
         self.demand_tanks = {}  # collection of tanks with actual demand values - for uncertainty analysis
         self.lp_model = None
@@ -44,27 +44,34 @@ class Simulation:
 
     def get_time_range(self):
         if isinstance(self.t1, datetime.datetime) and isinstance(self.t2, datetime.datetime):
-            tr = pd.date_range(start=self.t1, periods=self.duration, freq=str(self.hr_step_size) + "H")
-        if isinstance(self.t1, int) and isinstance(self.t2, int):
-            tr = pd.Series(np.arange(start=self.t1, stop=self.t2 + self.hr_step_size, step=self.hr_step_size))
+            tr = pd.date_range(start=self.t1, periods=self.duration, freq="H")
+        elif isinstance(self.t1, int) and isinstance(self.t2, int):
+            tr = pd.Series(np.arange(start=self.t1, stop=self.t2 + 1, step=1))
         return tr
 
-    def split_date_range(self, n):
-        date_range = pd.date_range(start=self.t1, periods=self.duration, freq='60min')
-        df = pd.DataFrame(index=date_range, columns=['tariff'])
+    def split_range(self, df, n):
         df.index.name = 'time'
-        df['tariff'] = df.apply(lambda x: utils.get_tariff(x.name, 'high')[0], axis=1)
-        df.reset_index(inplace=True)
-        df['group'] = df['tariff'].ne(df['tariff'].shift()).cumsum()
+        df['group'] = (df['name'].ne(df['name'].shift()) | (df['day'].ne(df['day'].shift()))).cumsum()
         df = df.groupby('group')
         temp = pd.DataFrame()
         for group, data in df:
             data.reset_index(inplace=True)
             data = data.groupby(data.index // n).first()
             temp = pd.concat([temp, data])
-
-        temp['step_size'] = temp['time'].diff()
+        temp['step_size'] = temp['time'].diff().shift(-1) / np.timedelta64(1, 'h')
+        temp.loc[temp.index[-1], 'step_size'] = (self.t2 - temp['time'].iloc[-1]) / np.timedelta64(1, 'h')
+        temp.index = temp['time']
         return temp
+
+    def match_to_sim_timeindex(self, df):
+        df.index = self.get_time_range()
+        temp = pd.DataFrame(index=self.date_tariff.index)
+        temp['group'] = range(len(temp))
+        df = pd.merge(temp, df, left_index=True, right_index=True, how='right').ffill()
+        df = df.groupby('group').sum()
+        df.index = temp.index
+        (df)
+        return df
 
     def get_num_steps(self):
         return len(self.time_range)
@@ -76,9 +83,9 @@ class Simulation:
             return self.t2 - self.t1
 
     def get_tariffs_vectors(self):
-        low_voltage_tariff = self.time_range.apply(lambda x: utils.get_tariff(x, 'low')[1])
-        high_voltage_tariff = self.time_range.apply(lambda x: utils.get_tariff(x, 'high')[1])
-        up_voltage_tariff = self.time_range.apply(lambda x: utils.get_tariff(x, 'up')[1])
+        low_voltage_tariff = self.time_range.apply(lambda x: electricity.get_tariff(x, 'low')[1])
+        high_voltage_tariff = self.time_range.apply(lambda x: electricity.get_tariff(x, 'high')[1])
+        up_voltage_tariff = self.time_range.apply(lambda x: electricity.get_tariff(x, 'up')[1])
         df = pd.DataFrame({'low': low_voltage_tariff, 'high': high_voltage_tariff, 'up': up_voltage_tariff})
         return df
 
@@ -108,7 +115,7 @@ class Simulation:
             step_size[-1] = self.t2 - temp.index.get_level_values(0).max()
             step_size = step_size / np.timedelta64(1, 'h')
             temp = pd.merge(temp, step_size, left_index=True, right_index=True)
-            temp = utils.separate_consecutive_tariffs(temp)
+            temp = separate_consecutive_tariffs(temp)
             temp['eps'] *= self.tariff_epsilon
             temp['cost'] = temp['flow'] * temp['se'] * temp['tariff'] * (1 + temp['eps'])
 
@@ -179,7 +186,8 @@ class Simulation:
             t.vars = pd.DataFrame(index=self.time_range)
             t.vars['cost'] = 0
             demands_file = os.path.join(self.data_folder, 'Demands', 'demands.' + str(int(t.zone)))
-            t.vars['demand'] = demands.load_from_csv(demands_file, self.t1, self.t2)
+            tank_demands = self.match_to_sim_timeindex(demands.load_from_csv(demands_file, self.t1, self.t2))
+            t.vars['demand'] = tank_demands
             t.vars = demands.demand_factorize(t.zone, t.vars, self.data_folder)
             if self.dynamic_min_vol:
                 self.get_dynamic_min_vol(t)
@@ -213,3 +221,15 @@ class Simulation:
 
     def get_results(self):
         self.results = Results(self)
+
+
+def separate_consecutive_tariffs(df):
+    df['group'] = df['tariff'].ne(df['tariff'].shift()).cumsum()
+    df['auxiliary'] = df['group'].diff().replace({0: np.nan})
+    df.iloc[[0, 0], df.columns.get_loc('auxiliary')] = 1
+    df['auxiliary'] = df['auxiliary'].ffill() + df.groupby(df['auxiliary'].notnull().cumsum()).cumcount()
+    df['eps'] = ((df['auxiliary'] - 1) / len(df.index.get_level_values('comb').drop_duplicates())).astype(int)
+    df = df.drop('auxiliary', axis=1)
+    return df
+
+
