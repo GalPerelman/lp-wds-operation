895a96d05a826d3ee1e9b9b54083562b8b213713
GalPerelman <peligur@gmail.com>
2023-04-16 20:14:49 +0300

    finalize adjustable robust optimization class

--- wdsoo/aropt.py
+++ wdsoo/aropt.py
@@ -1,7 +1,7 @@
-import os
 import pandas as pd
 import numpy as np
 import operator
+from scipy import sparse
 
 from rsome import ro
 import rsome as rso
@@ -17,10 +17,12 @@ CLP_STATUS = {-1: 'unknown', 0: 'OPTIMAL', 1: 'primal infeasible', 2: 'dual infe
 
 
 class ARO:
-    def __init__(self, sim, robustness: dict, uset_type):
+    def __init__(self, sim, robustness, lou, uset_type, worst_case=False):
         self.sim = sim
         self.robustness = robustness
+        self.lou = lou  # level of uncertainty
         self.uset_type = uset_type
+        self.worst_case = worst_case  # If True objective value for worst_case else nominal
 
         self.udata = uutils.init_uncertainty(self.sim)
         self.utanks = list(self.udata['demand'].elements.keys())
@@ -37,7 +39,7 @@ class ARO:
         self.build_mass_balance()
         self.vsp_volume()
         self.vsp_changes()
-        self.max_power()
+        # self.max_power()
         self.objective_func()
 
     def get_nominal_demands(self):
@@ -54,7 +56,7 @@ class ARO:
 
     def declare_random_variables(self):
         z = self.model.rvar(self.nominal_demands.shape)
-        z_set = rso.norm(z.reshape(-1), self.uset_type) <= self.robustness['demand']
+        z_set = rso.norm(z.reshape(-1), self.uset_type) <= self.robustness
         return z, z_set
 
     def get_x_idx(self, name):
@@ -72,36 +74,37 @@ class ARO:
             else:
                 c = 1
 
+            # adaption rule - all to all
             for t in range(1, self.sim.num_steps):
                 self.x[xmin_idx + t * c: xmin_idx + (t + 1) * c].adapt(self.z[:t, :])
 
     def range_constraints(self):
         for s_name, s in self.sim.network.pump_stations.items():
             xmin_idx, xmax_idx = self.get_x_idx(s_name)
-            self.model.st(self.x[xmin_idx: xmax_idx] >= 0)
-            self.model.st(self.x[xmin_idx: xmax_idx] <= 1)
+            self.model.st((self.x[xmin_idx: xmax_idx] >= 0).forall(self.z_set))
+            self.model.st((self.x[xmin_idx: xmax_idx] <= 1).forall(self.z_set))
 
         for vsp_name, vsp in self.sim.network.vsp.items():
             xmin_idx, xmax_idx = self.get_x_idx(vsp_name)
-            self.model.st(self.x[xmin_idx: xmax_idx] >= vsp.min_flow)
-            self.model.st(self.x[xmin_idx: xmax_idx] <= vsp.max_flow)
+            self.model.st((self.x[xmin_idx: xmax_idx] >= vsp.min_flow).forall(self.z_set))
+            self.model.st((self.x[xmin_idx: xmax_idx] <= vsp.max_flow).forall(self.z_set))
             if not np.isnan(vsp.init_flow):
-                self.model.st(self.x[xmin_idx] == vsp.init_flow)
+                self.model.st((self.x[xmin_idx] == vsp.init_flow).forall(self.z_set))
 
         for well_name, well in self.sim.network.wells.items():
             xmin_idx, xmax_idx = self.get_x_idx(well_name)
-            self.model.st(self.x[xmin_idx: xmax_idx] >= 0)
-            self.model.st(self.x[xmin_idx: xmax_idx] <= 1)
+            self.model.st((self.x[xmin_idx: xmax_idx] >= 0).forall(self.z_set))
+            self.model.st((self.x[xmin_idx: xmax_idx] <= 1).forall(self.z_set))
 
         for cv_name, cv in self.sim.network.control_valves.items():
             xmin_idx, xmax_idx = self.get_x_idx(cv_name)
-            self.model.st(self.x[xmin_idx: xmax_idx] >= 0)
-            self.model.st(self.x[xmin_idx: xmax_idx] <= 1)
+            self.model.st((self.x[xmin_idx: xmax_idx] >= 0).forall(self.z_set))
+            self.model.st((self.x[xmin_idx: xmax_idx] <= 1).forall(self.z_set))
 
         for v_name, v in self.sim.network.valves.items():
             xmin_idx, xmax_idx = self.get_x_idx(v_name)
-            self.model.st(self.x[xmin_idx: xmax_idx] >= 0)
-            self.model.st(self.x[xmin_idx: xmax_idx] <= 1)
+            self.model.st((self.x[xmin_idx: xmax_idx] >= 0).forall(self.z_set))
+            self.model.st((self.x[xmin_idx: xmax_idx] <= 1).forall(self.z_set))
 
     def comb_matrix(self, element, inout, param=None):
         """ return a matrix for elements with discrete hydraulic states (pumps combinations) """
@@ -145,7 +148,18 @@ class ARO:
             rows = np.hstack([np.repeat(i, len(station.combs)) for i in range(self.sim.num_steps)])
             cols = np.arange(len(station.combs) * self.sim.num_steps)
             matrix[rows, cols] = 1
-            self.model.st(matrix @ x <= (np.ones((self.sim.num_steps, 1))))
+            matrix = sparse.csr_matrix(matrix)
+            self.model.st((matrix @ x <= (np.ones((self.sim.num_steps, 1)))).forall(self.z_set))
+
+    def get_cumulative_flow_matrix(self, element_name, flow_direction):
+        element = self.sim.network.flow_elements[element_name]
+
+        if hasattr(element, 'combs'):
+            mat = self.cumulative_comb_matrix(element, flow_direction, param='flow')
+        else:
+            mat = self.cumulative_not_comb_matrix(flow_direction)
+
+        return mat
 
     def mass_balance(self, tanks: dict, affine_map=None):
         T = self.sim.num_steps
@@ -157,50 +171,40 @@ class ARO:
         lhs_init = np.zeros((ntanks * T, 1), dtype=float)
         rhs_min = np.zeros((ntanks * T, 1), dtype=float)
         rhs_max = np.zeros((ntanks * T, 1), dtype=float)
-        rhs_final = np.zeros((ntanks, 1), dtype=float)
-        b = np.zeros((ntanks * T, 1), dtype=float)
+        dem = np.zeros((ntanks * T, 1), dtype=float)
 
         for tank_idx, (tank_name, tank) in enumerate(tanks.items()):
-            b[tank_idx * T: (tank_idx + 1) * T] = tank.vars['demand'].cumsum().values.reshape(-1, 1)
-            mv = np.append(tank.min_vol[1:], tank.final_vol)
-            rhs_min[tank_idx * T: (tank_idx + 1) * T] = mv.reshape(-1, 1)
+            dem[tank_idx * T: (tank_idx + 1) * T] = tank.vars['demand'].cumsum().values.reshape(-1, 1)
+            rhs_min[tank_idx * T: (tank_idx + 1) * T] = np.append(tank.min_vol[1:], tank.final_vol).reshape(-1, 1)
             rhs_max[tank_idx * T: (tank_idx + 1) * T] = tank.max_vol
             lhs_init[tank_idx * T: (tank_idx + 1) * T] = tank.initial_vol
-            rhs_final[tank_idx] = tank.final_vol
 
             for element_name in self.sim.vars['name'].unique():
                 element = self.sim.network.flow_elements[element_name]
                 xmin_idx, xmax_idx = self.get_x_idx(element_name)
 
-                if element in tank.inflows + tank.vsp_inflows + tank.v_inflows + tank.cv_inflows:
-                    flow_direction = 'in'
-                elif element in tank.outflows + tank.vsp_outflows + tank.v_outflows + tank.cv_outflows:
-                    flow_direction = 'out'
+                if element in tank.inflows:
+                    mat = self.get_cumulative_flow_matrix(element_name, flow_direction='in')
+                elif element in tank.pumps_outflows + tank.vsp_outflows + tank.v_outflows + tank.cv_outflows:
+                    mat = self.get_cumulative_flow_matrix(element_name, flow_direction='out')
                 else:
                     continue
 
-                if hasattr(element, 'combs'):
-                    mat = self.cumulative_comb_matrix(element, flow_direction, param='flow')
-                else:
-                    mat = self.cumulative_not_comb_matrix(flow_direction)
-
                 lhs[tank_idx * T: (tank_idx + 1) * T, xmin_idx: xmax_idx] = mat
 
-        # final_idx = [i * T - 1 for i in range(1, ntanks + 1)]
         if affine_map is None:
-            self.model.st(lhs @ self.x <= np.squeeze(rhs_max + b - lhs_init))
-            self.model.st(lhs @ self.x >= np.squeeze(rhs_min + b - lhs_init))
-            # self.model.st(lhs[final_idx] @ self.x >= rhs_final + b[final_idx] - lhs_init[final_idx])
+            self.model.st((lhs @ self.x <= np.squeeze(rhs_max + dem - lhs_init)).forall(self.z_set))
+            self.model.st((lhs @ self.x >= np.squeeze(rhs_min + dem - lhs_init)).forall(self.z_set))
 
         else:
             self.model.st((lhs @ self.x <= np.squeeze(rhs_max)
-                           + np.squeeze(b)
-                           + affine_map @ self.z.reshape(-1)
+                           + (1 + self.lou * self.z.reshape(-1)) * np.squeeze(dem)
+                           # + affine_map @ self.z.reshape(-1)
                            - np.squeeze(lhs_init)).forall(self.z_set))
 
             self.model.st((lhs @ self.x >= np.squeeze(rhs_min)
-                           + np.squeeze(b)
-                           + affine_map @ self.z.reshape(-1)
+                           + (1 + self.lou * self.z.reshape(-1)) * np.squeeze(dem)
+                           # + affine_map @ self.z.reshape(-1)
                            - np.squeeze(lhs_init)).forall(self.z_set))
 
     def build_mass_balance(self):
@@ -235,7 +239,7 @@ class ARO:
             matrix = self.not_comb_matrix('in')
             matrix = np.multiply(matrix, vsp.vars['aux'].to_numpy()[:, np.newaxis])  # row-wise multiplication
             lhs = matrix.sum(axis=0) @ vsp_x  # sum of matrix rows to get the total flow for period
-            self.model.st(operators[operator_type](lhs, volume))
+            self.model.st((operators[operator_type](lhs, volume)).forall(self.z_set))
             vsp.vars = vsp.vars.drop('aux', axis=1)
 
     def vsp_changes(self):
@@ -261,7 +265,8 @@ class ARO:
             matrix[0, 0] = 0
 
             matrix = np.multiply(matrix, vsp.vars['aux'].to_numpy()[:, np.newaxis])  # row-wise multiplication
-            self.model.st(matrix @ vsp_x == 0)
+            matrix = sparse.csr_matrix(matrix)
+            self.model.st((matrix @ vsp_x == 0).forall(self.z_set))
 
     def max_power(self):
         df = pd.read_csv(self.sim.data_folder + '/power_stations.csv')
@@ -289,27 +294,59 @@ class ARO:
             xmin_idx, xmax_idx = self.get_x_idx(element_name)
             obj += (element.vars['cost'].values @ self.x[xmin_idx: xmax_idx]).sum()
 
-        self.model.minmax(obj)
+        if self.worst_case:
+            self.model.minmax(obj)
+
+        if not self.worst_case:
+            nominal_uset = (self.z == np.zeros(self.z.shape))
+            self.model.minmax(obj, nominal_uset)
 
     def solve(self):
-        self.model.solve(grb, display=True, params={'DualReductions': 0, 'CSClientLog': 3})
-        obj, x, status = self.model.solution.objval, self.model.solution.x, self.model.solution.status
+        self.model.solve(grb, display=False, params={'LogToConsole': 0, 'CSClientLog': 3})
+        obj, x, status = self.model.get(), self.model.solution.x, self.model.solution.status
         status = GRB_STATUS[status]
         self.get_results()
-        return obj, x, status
+        return status, obj
 
-    def get_results(self):
-        res = self.x.get()
+    def get_ldr_constants(self):
+        """ extract the constant of the linear decision rule
+            returns a matrix of constants with shape of x_shape X num_adaption_steps X num_adaption_elements
+        """
+        pi = self.x.get(self.z)
+        pi[np.isnan(pi)] = 0
+        return pi
+
+    def get_x_values(self, sample=None):
+        if sample is None:
+            sample = np.zeros(self.z.shape)
+        pi0 = self.x.get()
+        pi = self.get_ldr_constants()
+
+        pi = np.multiply(pi, sample)
+        for _ in range(len(pi.shape) - len(pi0.shape)):
+            pi = pi.sum(axis=-1)
+
+        return pi0 + pi
+
+    def retrieve_decision_vars(self, sample=None):
+        if sample is None:
+            sample = np.zeros(self.z.shape)
+
+        return self.x(self.z.assign(sample))
+
+    def get_results(self, sample=None):
+        x = self.retrieve_decision_vars(sample)
         for name in self.sim.vars['name'].unique():
             xmin_idx, xmax_idx = self.get_x_idx(name)
-            self.sim.network.flow_elements[name].vars['value'] = res[xmin_idx: xmax_idx]
+            self.sim.network[name].vars['value'] = x[xmin_idx: xmax_idx]
 
+        self.sim.vars['value'] = x
         self.tanks_balance()
 
     def tanks_balance(self):
         for t_name, t in self.sim.network.tanks.items():
             df = pd.DataFrame(index=self.sim.time_range, data={'df': 0})
-            for x in t.inflows + t.cv_inflows:
+            for x in t.pumps_inflows + t.cv_inflows:
                 qin = x.vars['flow'] * x.vars['value']
                 qin = qin.groupby(level='time').sum()
                 df = pd.merge(df, qin.rename(x.name), left_index=True, right_index=True)
@@ -318,7 +355,7 @@ class ARO:
                 qin = x.vars['value']
                 df = pd.merge(df, qin.rename(x.name), left_index=True, right_index=True)
 
-            for x in t.outflows + t.cv_outflows:
+            for x in t.pumps_outflows + t.cv_outflows:
                 qout = x.vars['flow'] * x.vars['value']
                 qout = -1 * qout.groupby(level='time').sum()
                 df = pd.merge(df, qout.rename(x.name), left_index=True, right_index=True)
@@ -332,3 +369,46 @@ class ARO:
             df['volume'] = t.initial_vol + (df['inflow'] + df['demand']).cumsum()
             t.vars['value'] = df['volume']
             t.vars['inflow'] = df['inflow']
+
+    def multivariate_normal(self, n_sim):
+        rng = np.random.default_rng()
+        sigma = self.udata['demand'].sigma
+        z = rng.multivariate_normal(np.zeros(sigma.shape[0]), sigma, size=n_sim, method='cholesky').T
+        return z
+
+    def set_vars_by_sample(self, single_sample):
+        self.get_results(single_sample)
+
+    def get_objective_by_sample(self):
+        return (self.sim.vars['cost'] * self.sim.vars['value']).sum()
+
+    def tank_vol_by_sample(self, tank_name, sample):
+        tank = self.sim.network[tank_name]
+        tank_vol = np.empty(sample.shape)
+        dem_sample = self.nominal_demands + sample
+        for j in range(dem_sample.shape[1]):
+            s = sample[:, j].reshape(-1, 1)
+            d = dem_sample[:, j].reshape(-1, 1)
+            self.set_vars_by_sample(s)
+            tank_inflow = self.sim.network.tanks[tank_name].vars['inflow'].values.reshape(-1, 1)
+            tank_vol[:, j] = (tank.initial_vol + (tank_inflow - d).cumsum(axis=0)).T
+
+        return tank_vol
+
+    def analyze_sample(self, sample):
+        dem_sample = self.nominal_demands + sample
+
+        costs = []
+        tanks_vol = {t_name: np.empty(sample.shape) for t_name, t in self.sim.network.tanks.items()}
+        for j in range(sample.shape[1]):
+            single_sample = sample[:, j].reshape(-1, 1)
+            single_dem_sample = dem_sample[:, j].reshape(-1, 1)
+            self.set_vars_by_sample(single_sample)
+
+            costs.append(self.get_objective_by_sample())
+
+            for tank_name, tank in self.sim.network.tanks.items():
+                tank_inflow = self.sim.network.tanks[tank_name].vars['inflow'].values.reshape(-1, 1)
+                tanks_vol[tank_name][:, j] = (tank.initial_vol + (tank_inflow - single_dem_sample).cumsum(axis=0)).T
+
+        return costs, tanks_vol
